07700a2f93e1559ff808d5a991319c36fdd776ff
Chiranth A M <chiranth.ajjamanemanohar@ucdenver.edu>
2025-02-23 16:37:27 -0700

    added new folder

---
+++ C and C++ Notes/Friend.cpp
@@ -0,0 +1,10 @@
+/*
+
+In C++, the friend class declaration can be placed anywhere inside the class—under the public, private, or protected section. 
+It doesn't matter where you put it because friendship is not affected by access specifiers.
+
+In C++, the friend keyword allows a class or function to access private and protected members of another class. 
+
+To provide more flexibility than inheritance when sharing data between classes.
+
+*/
