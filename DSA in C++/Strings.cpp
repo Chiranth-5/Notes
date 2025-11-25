857934ab5ef7847ed45cbaed146c1499189b3982
Chiranth A M <chiranth.ajjamanemanohar@ucdenver.edu>
2025-11-24 18:44:03 -0700

    added pair datastructure

--- DSA in C++/Strings.cpp
+++ DSA in C++/Strings.cpp
@@ -1,244 +1,137 @@
-// /*
-// Control Charachters c,s,d
+// #include <iostream>
+// #include <string>
+// using namespace std;
 
-// Format Specifiers for printf and scanf:
-// %d: Integer
-// %f: Float
-// %c: Character
-// %s: String
-// %x: Hexadecimal (lowercase)
-// %X: Hexadecimal (uppercase)
-// %o: Octal
-// %e or %E: Scientific Notation
-// %p: Pointer Address
+// // -------------------- Character Array Functions --------------------
 
-
-// Char temp;
-// temp = ‘A’;
-// temp = “A”;
-// temp = ‘AB’;
-// temp = A;
-// printf( “%c” , temp ); // in C language
-// Cout<<temp; // in c++ langage
-// • A character Array can be define and initialise as follows
-// char x[ 5 ] ;
-// char x[ 5 ] = {‘A’, ‘B’, ‘C’, ‘D’, ‘E’};
-// char x[ ] = {‘A’, ‘B’, ‘C’, ‘D’, ‘E’};
-// char x[5] = { 65,66,67,68,69};
-// char x[5] = {‘A’ , ‘B’};
-// • An array of characters are nothing but strings
-// • \0 is used to define the end of string or end of character , it is also
-// know as string delimiter, end of string char , NULL char or string
-// terminator
-// • Creating and initialising of string can be done as follows
-// char name[ 10 ] = {‘J’, ‘O’, ‘H’, ‘N’, ‘\0’};
-// char name[ ] = {‘J’, ‘O’, ‘H’, ‘N’, ‘\0’};
-// char name[ ] = {‘’John”};
-
-// */
-
-// #include<iostream>
-// #include<string>
-
-
+// // Calculate length of char array
 // void length(char A[])
 // {
-//     int i=0;
-//     for( i=0; A[i]!='\0' ; i++)
-//     {
-    
-//     }
-//     std::cout << i << std::endl;
+//     int i = 0;
+//     while(A[i] != '\0') i++;
+//     cout << "Length: " << i << endl;
 // }
 
+// // Change case: uppercase <-> lowercase
 // void changingCase(char C[])
 // {
+//     for(int i = 0; C[i] != '\0'; i++)
+//     {
+//         if(C[i] >= 'A' && C[i] <= 'Z')
+//             C[i] = C[i] + 32;
+//         else if(C[i] >= 'a' && C[i] <= 'z')
+//             C[i] = C[i] - 32;
+//     }
+//     cout << "After changing case: " << C << endl;
+// }
+
+// // Count vowels and consonants
+// void vowelsAndConsonants(char C[])
+// {
+//     int vowels = 0, consonants = 0;
+//     for(int i = 0; C[i] != '\0'; i++)
+//     {
+//         char ch = C[i];
+//         if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+//         {
+//             if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+//                ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+//                 vowels++;
+//             else
+//                 consonants++;
+//         }
+//     }
+//     cout << "Vowels: " << vowels << ", Consonants: " << consonants << endl;
+// }
+
+// // Count number of words
+// void countWords(char C[])
+// {
+//     int count = 0;
 //     for(int i=0; C[i]!='\0'; i++)
 //     {
-//         if(C[i]>=65 && C[i]<=90)
-//         {
-//             C[i] = C[i] + 32;
-//         }
-//         else if (C[i]>=97 && C[i]<=122)
-//         {
-//             C[i] = C[i] - 32;
-//         }
-//     }
-//     std::cout << C << std::endl;
-// }
-
-// void vowelsandConsonant(char C[])
-// {
-//     int count=0;
-//     int i=0;
-//     for( i=0; C[i]!='\0'; i++)
-//     {
-//         if(C[i] == 'a' || C[i] == 'e' || C[i] == 'i' || C[i] == 'o' || C[i] == 'u' ||
-//            C[i] == 'A' || C[i] == 'E' || C[i] == 'I' || C[i] == 'O' || C[i] == 'U')
-//         {
-//             count++;
-//         }
-//     } 
-//     std::cout << "Number of Vowels : " << count << std::endl;
-
-//     int whitespace = 0;
-//     for( int j=0; C[j]!='\0'; j++)
-//     {
-//         if( !((C[j]>=65 && C[j]<=90) || (C[j]>=97 && C[j]<=122)))
-//         {
-//             whitespace++;
-//         }
-//     }
-//     std::cout << "Number of Consonantes : " << i - count - whitespace << std::endl;
-
-// }
-
-// void countingNumberOfWords(char C[])
-// {
-//     int count=0;
-//     for( int i=0; C[i]!='\0'; i++)
-//     {
 //         if(C[i]==' ' && C[i-1]!=' ')
-//         {
 //             count++;
-//         }
 //     }
-//     std::cout << "Number of words : " << count+1 << std::endl;
+//     cout << "Number of words: " << count + 1 << endl;
 // }
 
-// void reverseingString2methods(char C[])
+// // Reverse string using extra array and in-place
+// void reverseString(char C[])
 // {
-//     int i=0;
-//     for( i=0; C[i]!='\0' ; i++)
-//     {
-    
-//     }
-//     char B[i+1];
-//     i--;
-//     int j;
-//     for( j=0; i>=0 ; i--,j++)
-//     {
-//         B[j] = C[i];
-//     }
-//     B[j] ='\0';
-//     std::cout << "Reversed string using extra ArrayOfStrings : " << B << std::endl;
+//     // Extra array
+//     int len = 0;
+//     while(C[len] != '\0') len++;
+//     char B[len+1];
+//     for(int i=0; i<len; i++)
+//         B[i] = C[len-1-i];
+//     B[len] = '\0';
+//     cout << "Reversed (extra array): " << B << endl;
 
-//     int k=0;
-//     for( ; C[k]!='\0' ; k++)
+//     // In-place
+//     for(int i=0, j=len-1; i<j; i++, j--)
 //     {
-    
+//         char temp = C[i];
+//         C[i] = C[j];
+//         C[j] = temp;
 //     }
-//     int l = k-1;
-//     for( int m=0; m<l ; l--,m++)
-//     {
-//         char temp;
-//         temp = C[m];
-//         C[m] = C[l];
-//         C[l] = temp;
-//     }
-//     std::cout << "Reversed string without extra ArrayOfStrings : " << C << std::endl;
-
+//     cout << "Reversed (in-place): " << C << endl;
 // }
 
-// void duplicatesUsingBitWiseOperator(char A[])
+// // -------------------- std::string append() Examples --------------------
+// void stringAppendExamples()
 // {
-//     //size of int is 4 bytes
-//     int x=0;
-//     int y=0;
+//     cout << "\n--- std::string append() Examples ---\n";
 
-//     for (int i=0; A[i]!='\0'; i++)
-//     {
-//         x=1;
-//         x = x<<(A[i]-97);
-//         if ((x&y) >0)
-//         {
-//             std::cout << A[i] << std::endl;
-//         }
-//         else
-//         {
-//             y = (x|y);
-//         }
-//     }
+//     string s;
+
+//     // 1. append(count, char)
+//     s = "A";
+//     s.append(3, 'x');   // "Axxx"
+//     cout << "1. append(count, char): " << s << endl;
+
+//     // 2. append(const char* cstring)
+//     s = "hi ";
+//     s.append("there");  // "hi there"
+//     cout << "2. append(cstring): " << s << endl;
+
+//     // 3. append(const char* cstring, size_t count)
+//     s = "start ";
+//     s.append("abcdef", 3);  // "start abc"
+//     cout << "3. append(cstring, count): " << s << endl;
+
+//     // 4. append(const string& str)
+//     string A = "new";
+//     string B = "new";
+//     A.append(B);   // "newnew"
+//     cout << "4. append(string): " << A << endl;
+
+//     // 5. append(const string& str, size_t pos)
+//     A = "new";
+//     A.append(B, 2);  // append B.substr(2) -> "w" => "neww"
+//     cout << "5. append(string, pos): " << A << endl;
+
+//     // 6. append(const string& str, size_t pos, size_t count)
+//     A = "new";
+//     A.append(B, 0, 2);  // append first 2 chars -> "ne" => "newne"
+//     cout << "6. append(string, pos, count): " << A << endl;
 // }
 
-// void isAnagram(char A[],char B[])
-// {
-//     char H[26] = {};
-
-//     for(int i=0; A[i]!='\0'; i++)
-//     {
-//         H[A[i]-97]++;
-//     }
-//     int i=0;
-//     for( ; B[i]!='\0'; i++)
-//     {
-//         H[B[i]-97]--;
-//         if(H[B[i]-97] < 0)
-//         {
-//             std::cout << "Its not Anagram" << std::endl;
-//             break;
-//         }
-//     }
-//     if(B[i]=='\0')
-//     {
-//         std::cout << "Its Anagram" << std::endl;
-//     }
-// }
-
-// void perm(char D[], int k)
-// {
-//     static int A[10] ={0};
-//     static char Res[10] ;
-//     int i;
-
-//     if (D[k] == '\0')
-//     {
-//         Res[k] = '\0';
-//         std::cout << Res << std::endl;
-//     }
-//     else
-//     {
-//         for(int i=0; D[i]!='\0'; i++)
-//         {
-//             if(A[i]==0)
-//             {
-//                 Res[k] = D[i];
-//                 A[i]=1;
-//                 perm(D,k+1);
-//                 A[i]=0;
-//             }
-//         }
-//     }
-// }
-
+// // -------------------- Main --------------------
 // int main()
 // {
-//     // // char A[4] ={'A','B','C','\0'};
-//     // // printf( "%s", A);
-//     // // std::cout << A << std::endl;
-//     // char C[] = "Chiranth is good";
-//     // char A[] = "medical";
-//     // char B[] = "decimal";
-//     // char D[] = "ABC";
-//     // int k=0;
- 
-//     // // printf( "%s\n", C);
-//     // // std::cout << C << std::endl;
-//     // // char B = 'B';
-//     // // printf( "%c", B);
-//     // // std::cout << B << std::endl;
+//     // Character array examples
+//     char arr[] = "Hello World";
 
-//     // // length(C);
-//     // // changingCase(C);
-//     // // vowelsandConsonant(C);
-//     // // countingNumberOfWords(C);
-//     // // reverseingString2methods(C);
-//     // // comparingStrings();
-//     // // duplicatesInString();Using hastable of length 0-25
-//     // // duplicatesUsingBitWiseOperator(A);
-//     // // isAnagram(A,B);
-//     // perm(D,k);
-    
-//     std::string A;
-//     std::cout << A.size() << std::endl;
+//     cout << "--- Character Array Examples ---\n";
+//     length(arr);
+//     changingCase(arr);
+//     vowelsAndConsonants(arr);
+//     countWords(arr);
+//     reverseString(arr);
+
+//     // String append examples
+//     stringAppendExamples();
+
+//     return 0;
 // }
--- DSA in C++/pair.cpp
+++ DSA in C++/pair.cpp
@@ -1,102 +1,102 @@
-#include <iostream>
-#include <utility>
-using namespace std;
+// #include <iostream>
+// #include <utility>
+// using namespace std;
 
-// Display set elements
-// void displaySet(const set<int> &lSet)
+// // Display set elements
+// // void displaySet(const set<int> &lSet)
+// // {
+// //     for (set<int>::iterator it = lSet.begin(); it != lSet.end(); it++)
+// //     {
+// //         cout << *it << endl;
+// //     }
+// // }
+
+// int main()
 // {
-//     for (set<int>::iterator it = lSet.begin(); it != lSet.end(); it++)
-//     {
-//         cout << *it << endl;
-//     }
-// }
+//     pair<int,int> pairDemo;
 
-int main()
-{
-    pair<int,int> pairDemo;
+//     /*
+//     Always inserted in sorted order.
+//     No duplicates allowed.
+//     Implemetation: Self balancing tree. RedBlack tree.
+//     */
 
-    /*
-    Always inserted in sorted order.
-    No duplicates allowed.
-    Implemetation: Self balancing tree. RedBlack tree.
-    */
+//     // Insert elements
+//     // Time Complexity: O(log n) per insert
+//     // no inbuilt functions.
 
-    // Insert elements
-    // Time Complexity: O(log n) per insert
-    // no inbuilt functions.
-
-    pairDemo.first = 5;
-    pairDemo.second = 6;
+//     pairDemo.first = 5;
+//     pairDemo.second = 6;
 
 
     
 
-    cout << "Elements in pair: " << endl;
-    std::cout << pairDemo.first;
-    std::cout << pairDemo.second;
+//     cout << "Elements in pair: " << endl;
+//     std::cout << pairDemo.first;
+//     std::cout << pairDemo.second;
 
-    // /*
-    // Common operations:
+//     // /*
+//     // Common operations:
 
-    // insert(value)      --- inserts the value in sorted order. O(log n)
-    // begin()            --- returns iterator to first element. O(1)
-    // end()              --- returns iterator to past-the-end. O(1)
-    // find(value)        --- returns iterator to value. O(log n)
-    // erase(value)       --- erase by value. O(log n)
-    // erase(iterator)    --- erase by iterator. O(log n)
-    // erase(first,last)  --- erase range. O(k + log n), k = range length
-    // count(value)       --- returns 0 or 1. O(log n)
-    // size()             --- returns number of elements. O(1)
-    // empty()            --- check if set is empty. O(1)
-    // clear()            --- removes all elements. O(n)
-    // */
+//     // insert(value)      --- inserts the value in sorted order. O(log n)
+//     // begin()            --- returns iterator to first element. O(1)
+//     // end()              --- returns iterator to past-the-end. O(1)
+//     // find(value)        --- returns iterator to value. O(log n)
+//     // erase(value)       --- erase by value. O(log n)
+//     // erase(iterator)    --- erase by iterator. O(log n)
+//     // erase(first,last)  --- erase range. O(k + log n), k = range length
+//     // count(value)       --- returns 0 or 1. O(log n)
+//     // size()             --- returns number of elements. O(1)
+//     // empty()            --- check if set is empty. O(1)
+//     // clear()            --- removes all elements. O(n)
+//     // */
 
-    // // Time Complexity: O(log n)
-    // set<int>::iterator itSample = setDemo.find(10);
-    // cout << endl;
-    // cout << *itSample << endl;
-    // cout << endl;
+//     // // Time Complexity: O(log n)
+//     // set<int>::iterator itSample = setDemo.find(10);
+//     // cout << endl;
+//     // cout << *itSample << endl;
+//     // cout << endl;
 
-    // // erase by value
-    // // Time Complexity: O(log n)
-    // setDemo.erase(5);  // remove 5
-    // displaySet(setDemo);
-    // cout << endl;
+//     // // erase by value
+//     // // Time Complexity: O(log n)
+//     // setDemo.erase(5);  // remove 5
+//     // displaySet(setDemo);
+//     // cout << endl;
 
-    // // erase value not present
-    // // Time Complexity: O(log n) - nothing erased
-    // setDemo.erase(300);
-    // displaySet(setDemo);
-    // cout << endl;
+//     // // erase value not present
+//     // // Time Complexity: O(log n) - nothing erased
+//     // setDemo.erase(300);
+//     // displaySet(setDemo);
+//     // cout << endl;
 
-    // // erase by iterator
-    // // Time Complexity: O(log n)
-    // itSample = setDemo.find(20);
-    // setDemo.erase(itSample);
-    // displaySet(setDemo);
-    // cout << endl;
+//     // // erase by iterator
+//     // // Time Complexity: O(log n)
+//     // itSample = setDemo.find(20);
+//     // setDemo.erase(itSample);
+//     // displaySet(setDemo);
+//     // cout << endl;
 
-    // // erase by range in iterator
-    // // Time Complexity: O(k + log n), k = number of elements erased
-    // itSample = setDemo.find(50);
-    // setDemo.erase(itSample, setDemo.end());
-    // displaySet(setDemo);
-    // cout << endl;
+//     // // erase by range in iterator
+//     // // Time Complexity: O(k + log n), k = number of elements erased
+//     // itSample = setDemo.find(50);
+//     // setDemo.erase(itSample, setDemo.end());
+//     // displaySet(setDemo);
+//     // cout << endl;
 
-    // // size() --- O(1)
-    // cout << "Size of set: " << setDemo.size() << endl;
+//     // // size() --- O(1)
+//     // cout << "Size of set: " << setDemo.size() << endl;
 
-    // // empty() --- O(1)
-    // cout << "Is set empty? " << setDemo.empty() << endl;
+//     // // empty() --- O(1)
+//     // cout << "Is set empty? " << setDemo.empty() << endl;
 
-    // // count(value) --- O(log n)
-    // cout << "Is 20 present? " << setDemo.count(20) << endl;
-    // cout << "Is 100 present? " << setDemo.count(100) << endl;
+//     // // count(value) --- O(log n)
+//     // cout << "Is 20 present? " << setDemo.count(20) << endl;
+//     // cout << "Is 100 present? " << setDemo.count(100) << endl;
 
-    // // clear() --- O(n)
-    // setDemo.clear();
-    // cout << "After clear, size of set: " << setDemo.size() << endl;
-    // cout << "Is set empty now? " << setDemo.empty() << endl;
+//     // // clear() --- O(n)
+//     // setDemo.clear();
+//     // cout << "After clear, size of set: " << setDemo.size() << endl;
+//     // cout << "Is set empty now? " << setDemo.empty() << endl;
 
-    return 0;
-}
+//     return 0;
+// }
