6804b93ea3fd4aa6f7fadf872d638b997a839d33
Chiranth A M <chiranth.ajjamanemanohar@ucdenver.edu>
2026-02-05 11:06:40 -0700

    bfs and dfs completed

--- DSA in C++/Graphs-strivers.cpp
+++ DSA in C++/Graphs-strivers.cpp
@@ -2,25 +2,29 @@
 
 #include <iostream>
     using std::cout;
+    using std::endl;
 #include <vector>
     using std::vector;
 #include <queue>
     using std::queue;
 
 
+
 void bfsHelper( int startNode , vector<int>& vis, vector<int>& ans , vector<vector<int>>& adj)
 {
-    // compute bsf of connected graph
+    // 1.bfs of each node. Push all neigbours to queue and do bfs on each node in the queuue till its empty.
+    
 
-    queue<int> q; // for bfs. explore the way its visted
+    queue<int> q; 
 
+    vis[startNode] = 1;
     q.push(startNode);
 
     while( !q.empty())
     {
         int curr = q.front();
         q.pop();
-        ans.push_back(curr); 
+        ans.push_back(curr);
 
         // add the niegbours to queue
         vector<int> niegbour = adj[curr];
@@ -43,7 +47,7 @@ void bfsHelper( int startNode , vector<int>& vis, vector<int>& ans , vector<vect
 
 vector<int> bfsOfGraph( int v, vector<vector<int>> adj)
 {
-    // for each connected componeent run bfs with bfs helper.
+    // 1. Bfs of all disconnectd components.
 
     vector<int> vis(v,0);
     vector<int> ans;
@@ -59,6 +63,40 @@ vector<int> bfsOfGraph( int v, vector<vector<int>> adj)
     return ans;
 }
 
+void dfsHelper( int startNode, vector<int>& vis , vector<int>& ans , vector<vector<int>>& adj )
+{
+    //1.mark as visited and push to ans.
+    //2.depth first search of the start niegbour element
+    vis[startNode] = 1;
+    ans.push_back(startNode);
+
+    vector<int> neighbour = adj[startNode];
+    for( int node : neighbour)
+    {
+        if( vis[node] == 0) // not visited
+        {
+            dfsHelper( node, vis, ans, adj);
+        }
+    }
+}
+
+
+vector<int> dfsOfGraph( int v, vector<vector<int>> adj)
+{
+    // for each connected componeent run bfs with bfs helper.
+
+    vector<int> vis(v,0);
+    vector<int> ans;
+    int startNode = 0;
+
+    dfsHelper( startNode, vis, ans, adj);
+
+    return ans;
+}
+
+
+
+
 int main()
 {
     // graph  adjacency matrix representation
@@ -71,13 +109,21 @@ int main()
     adj[3] = {1};
     adj[4] = {2};
 
-    vector<int> result = bfsOfGraph(v, adj);
+    vector<int> resultOfBfs = bfsOfGraph(v, adj);
 
-    for( int node : result)
+    for( int node : resultOfBfs)
     {
         cout << node << " ";
     }
 
+    cout<<endl<<endl;
+    
+    vector<int> resultOfDfs = dfsOfGraph(v, adj);
+
+    for( int node : resultOfDfs)
+    {
+        cout << node << " ";
+    }
 
 }
 
